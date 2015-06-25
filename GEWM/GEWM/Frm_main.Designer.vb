fceeb3239459404fed67eda72e642dbcb7051175
aun <dev.chiangmai@gmail.com>
2015-06-25 21:41:03 +0700

    สร้างโปรเจ็คและฟอร์มหลัก

---
+++ GEWM/GEWM/Frm_main.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Frm_main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Frm_main))
+        Me.C1Ribbon1 = New C1.Win.C1Ribbon.C1Ribbon()
+        Me.RibbonApplicationMenu1 = New C1.Win.C1Ribbon.RibbonApplicationMenu()
+        Me.RibbonConfigToolBar1 = New C1.Win.C1Ribbon.RibbonConfigToolBar()
+        Me.RibbonQat1 = New C1.Win.C1Ribbon.RibbonQat()
+        Me.RibbonTab1 = New C1.Win.C1Ribbon.RibbonTab()
+        Me.RibbonGroup1 = New C1.Win.C1Ribbon.RibbonGroup()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        CType(Me.C1Ribbon1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'C1Ribbon1
+        '
+        Me.C1Ribbon1.ApplicationMenuHolder = Me.RibbonApplicationMenu1
+        Me.C1Ribbon1.ConfigToolBarHolder = Me.RibbonConfigToolBar1
+        Me.C1Ribbon1.Location = New System.Drawing.Point(0, 0)
+        Me.C1Ribbon1.Name = "C1Ribbon1"
+        Me.C1Ribbon1.QatHolder = Me.RibbonQat1
+        Me.C1Ribbon1.Size = New System.Drawing.Size(704, 156)
+        Me.C1Ribbon1.Tabs.Add(Me.RibbonTab1)
+        '
+        'RibbonApplicationMenu1
+        '
+        Me.RibbonApplicationMenu1.Name = "RibbonApplicationMenu1"
+        '
+        'RibbonConfigToolBar1
+        '
+        Me.RibbonConfigToolBar1.Name = "RibbonConfigToolBar1"
+        '
+        'RibbonQat1
+        '
+        Me.RibbonQat1.Name = "RibbonQat1"
+        '
+        'RibbonTab1
+        '
+        Me.RibbonTab1.Groups.Add(Me.RibbonGroup1)
+        Me.RibbonTab1.Name = "RibbonTab1"
+        Me.RibbonTab1.Text = "Tab"
+        '
+        'RibbonGroup1
+        '
+        Me.RibbonGroup1.Name = "RibbonGroup1"
+        Me.RibbonGroup1.Text = "Group"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 367)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(704, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Frm_main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(704, 389)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.C1Ribbon1)
+        Me.IsMdiContainer = True
+        Me.MaximizeBox = False
+        Me.Name = "Frm_main"
+        Me.Text = "ระบบบริหารกรอบอัตรากำลังลูกจ้างภาครัฐ กรณีศึกษาสำนักงานการตรวจเงินแผ่นดิน"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        CType(Me.C1Ribbon1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents C1Ribbon1 As C1.Win.C1Ribbon.C1Ribbon
+    Friend WithEvents RibbonApplicationMenu1 As C1.Win.C1Ribbon.RibbonApplicationMenu
+    Friend WithEvents RibbonConfigToolBar1 As C1.Win.C1Ribbon.RibbonConfigToolBar
+    Friend WithEvents RibbonQat1 As C1.Win.C1Ribbon.RibbonQat
+    Friend WithEvents RibbonTab1 As C1.Win.C1Ribbon.RibbonTab
+    Friend WithEvents RibbonGroup1 As C1.Win.C1Ribbon.RibbonGroup
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+
+End Class
---
+++ GEWM/GEWM/Frm_main.vb
@@ -0,0 +1,6 @@
+﻿Public Class Frm_main
+
+    Private Sub Frm_main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
